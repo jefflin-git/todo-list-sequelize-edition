81955989781ff80383118f31d73b3937397dfd4d
jefflin <linjef0711@gmail.com>
2021-01-26 21:59:23 +0800

    feat : add home page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
 router.get('/', (req, res) => {
-  res.send('test')
+  Todo.findAll({ raw: true, nest: true })
+    .then(todos => res.render('index', { todos }))
+    .catch((error => {
+      res.render('error', { message: 'error !' })
+      return res.status(422).json(error)
+    }))
 })
 
 module.exports = router
