a2b5a2b52417c37e2be64ba4d4aa6ae97894b74c
jefflin <linjef0711@gmail.com>
2021-01-27 11:51:48 +0800

    feat : add facebook login & dotenv

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = express.Router()
 const home = require('../routes/modules/home')
 const users = require('../routes/modules/users')
 const todos = require('../routes/modules/todos')
+const auth = require('../routes/modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 router.use('/todos', authenticator, todos)
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
