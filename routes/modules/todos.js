c7121978c29855f8b1b368ffa64359ad7defd968
jefflin <linjef0711@gmail.com>
2021-01-27 09:35:14 +0800

    feat : add detail page

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
-
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => {
+      console.log(error)
+      res.render('error', { message: 'error !' })
+    })
+})
 
 module.exports = router
