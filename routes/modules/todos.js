a088fad416a3976034009e84b61df239973352ba
jefflin <linjef0711@gmail.com>
2021-01-27 13:17:01 +0800

    feat : add edit page and edit feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -19,6 +19,33 @@ router.post('/', (req, res) => {
     })
 })
 
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => {
+      res.render('error', { message: 'error !' })
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => {
+      console.log(error)
+      res.render('error', { message: 'error !' })
+    })
+})
+
 router.delete('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
