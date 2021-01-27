6b70f9b1c27916b2d1f1df895418f3f3342f12fd
jefflin <linjef0711@gmail.com>
2021-01-27 10:47:26 +0800

    feat: modify user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs')
 const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
@@ -17,9 +18,35 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
+  const errors = []
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！' })
+  }
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '這個 Email 已經註冊過了。' })
+        return res.render('register', { errors, name, email, password, confirmPassword })
+      }
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name, email, password: hash
+        }))
+        .then(() => res.redirect('/'))
+        .catch(error => {
+          console.log(error)
+          res.render('error', { message: 'error !' })
+        })
+    })
 })
 
 router.get('/logout', (req, res) => {
