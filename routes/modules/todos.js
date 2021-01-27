28b10df5f23f4c68d9d9bec3d7a511a4039e5e7a
jefflin <linjef0711@gmail.com>
2021-01-27 12:59:27 +0800

    feat : add new page and create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -8,10 +8,21 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const UserId = req.user.id
+  Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => {
+      console.log(error)
+      res.render('error', { message: 'error !' })
+    })
+})
+
 router.get('/:id', (req, res) => {
   const id = req.params.id
   const UserId = req.user.id
-  Todo.Todo.findOne({ where: { id, UserId } })
+  Todo.findOne({ where: { id, UserId } })
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
     .catch(error => {
       console.log(error)
